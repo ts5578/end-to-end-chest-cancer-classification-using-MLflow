96acbdcd5c8cae0fcf96756ed274eed56c13ca88
Tanisha Bhol <ts5578@srmist.edu.in>
2024-08-14 00:03:02 +0530

    utils exception and logger added

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import sys
-sys.path.insert(0, 'C:\Users\ASUS\OneDrive\Desktop\end-to-end-chest-cancer-classification-using-MLflow\src\chest-cancer-classifier')
+from chest_cancer_classifier import logger
 
-from __init__.py import logger
-
-
-logger.info("welcome")
+logger.info("welcome to chest cancer classifier")
