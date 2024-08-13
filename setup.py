345e26c7998606aad03fa2b6c0c3508986de0ed9
Tanisha Bhol <ts5578@srmist.edu.in>
2024-08-13 21:58:47 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "end-to-end-chest-cancer-classification-using-MLflow"
+AUTHOR_USER_NAME = "ts5578"
+SRC_REPO = "chest-cancer-classifier"
+AUTHOR_EMAIL = "ts5578@srmist.edu.in"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
