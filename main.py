a3ec2f8681a6266dfc055216a2ee6a2f285e6d19
Tanisha Bhol <ts5578@srmist.edu.in>
2024-08-14 14:35:00 +0530

    model trainer added

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from chest_cancer_classifier import logger
 from chest_cancer_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from chest_cancer_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline 
-
+from chest_cancer_classifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline 
 
 
 STAGE_NAME ="Data Ingestion Stage"
@@ -23,6 +23,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
