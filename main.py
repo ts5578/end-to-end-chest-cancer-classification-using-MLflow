7d6f9f45a6c34cf8441a2d82a5b115f8eed35233
Tanisha Bhol <ts5578@srmist.edu.in>
2024-08-14 09:30:08 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from chest_cancer_classifier import logger
+from chest_cancer_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to chest cancer classifier")
+STAGE_NAME ="Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/chest_cancer_classifier/constants/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+#onfig_path = Path('config/config.yaml').resolve()
+
+CONFIG_FILE_PATH = Path("config\config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
