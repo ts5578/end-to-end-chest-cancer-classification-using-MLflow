e828e60bf3975a8e35d83bb55caad22e2de78d06
Tanisha Bhol <ts5578@srmist.edu.in>
2024-08-14 11:20:13 +0530

    prepare base model added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from chest_cancer_classifier import logger
 from chest_cancer_classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from chest_cancer_classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline 
+
+
 
 STAGE_NAME ="Data Ingestion Stage"
 
@@ -11,3 +14,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
